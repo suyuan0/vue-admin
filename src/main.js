fcfc0ef0e7d233a56bf2775503654512d340c0da
培培 <614963845@qq.com>
2022-07-05 11:16:40 +0800

    feat(beforeEach): 页面的鉴权

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './styles/index.scss'
+import './permission'
 
 console.log(process.env.VUE_APP_URL)
 const app = createApp(App)
---
+++ src/permission.js
@@ -0,0 +1,14 @@
+import router from '@/router'
+import store from '@/store'
+
+router.beforeEach((to, from, next) => {
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    next('/login')
+  }
+})
