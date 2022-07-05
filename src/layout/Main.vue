7ee86889d509a83ce31d16ff3d9591320e47c090
培培 <614963845@qq.com>
2022-07-05 11:04:58 +0800

    feat(layout): 创建layout布局

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    main
   </div>
 </template>
 
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-
+  <div class='app-main-container'>
+    <SideBar></SideBar>
+    <div class='main'>
+      <NavBar></NavBar>
+      <Main></Main>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import SideBar from './Sidebar'
+import NavBar from '@/layout/NavBar'
+import Main from '@/layout/Main'
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
+.app-main-container {
 
+}
 </style>
