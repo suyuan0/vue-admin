ee8d30a4270ae7f64f79d9eac13feec9d3c42dbd
培培 <614963845@qq.com>
2022-07-05 09:19:34 +0800

    chore(normalize): 引入公共样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import './styles/index.scss'
 
 const app = createApp(App)
 app.use(ElementPlus, {
